0081d6dc84e94cefcf41593fe1c804d35d9c4762
admin <contact@mabel.li>
2019-02-06 18:10:30 +0000

    Adds integration test for finding user current location and calculating distance between two sets of long/lat

--- test_integration_search.py
+++ test_integration_search.py
@@ -1,16 +1,30 @@
 from search import *
+from distance import distance
+import os
+import unittest
+from unittest.mock import patch
 
-# class TestSearch():
+# Tests function for finding user's current location, returning long/lat for hard-coded user input
+def test_location():
+    with patch("builtins.input", return_value = "SE17 2HP"):
+        if currentLocation() == {'longitude': -0.091014, 'latitude': 51.486879}:
+            return True, {'longitude': -0.091014, 'latitude': 51.486879}
+        else:
+            print('Failed to find current location')
+            return False
 
-def test_everything():
-    # call all the functions
-    # print(check_db('/Users/Me/Desktop/btfurther/module2/phonebook_project/phonebook_database.db'))
-    
-    # print(connect_db('/Users/Me/Desktop/btfurther/module2/phonebook_project/phonebook_database.db'))
-    main()
+# Tests the function for calculating distance between 2 sets of long/lat based on test_location output
+def test_distance():
+    if test_location:
+        x = test_location()
+        if distance(x[1]['latitude'], x[1]['longitude'], x[1]['latitude'], x[1]['longitude']) == 0:
+            return True
+        else:
+            print('Failed to calculate distance correctly')
+            return False
+
 
 if __name__ == "__main__":
-    # print(test_everything())
-  
-    test_everything()
+    print(test_location())
+    print(test_distance())
     
